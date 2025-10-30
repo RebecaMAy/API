6a5dc2fbc24aa5a5389748bc1f063b7db86aa842
Rebeca Mendez <mrmenaya@upv.edu.es>
2025-10-30 21:59:19 +0100

    cors

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from recursos.registro import Registro, Login
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+CORS(app)
 
 api = Api(app)
 
